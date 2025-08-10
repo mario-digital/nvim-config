002fc5f68e1c383a0dbf0cbd210e10f891b7c408
Mario Rodriguez <me@mario.life>
2025-08-10 04:19:05 -0700

    fix: Replace Find File icon with emoji for consistency

--- lua/mario/plugins/alpha.lua
+++ lua/mario/plugins/alpha.lua
@@ -21,7 +21,7 @@ return {
     dashboard.section.buttons.val = {
       dashboard.button("e", "📝 > New File", "<cmd>ene<CR>"),
       dashboard.button("SPC ee", "📁 > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-      dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("SPC ff", "📄 > Find File", "<cmd>Telescope find_files<CR>"),
       dashboard.button("SPC fs", "🔍 > Find Word", "<cmd>Telescope live_grep<CR>"),
       dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
       dashboard.button("q", "⏻ > Quit NVIM", "<cmd>qa<CR>"),
