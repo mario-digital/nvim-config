61813869c050db7dc8619fcca27857b664c59494
Mario Rodriguez <me@mario.life>
2025-08-10 02:48:54 -0700

    fix: Resolve keymap conflict between Flash and Substitute
    - Change Substitute keymaps from s to gs (go substitute)
    - Frees up 's' for Flash.nvim jump navigation
    - New substitute mappings: gs, gss, gS

--- lua/mario/plugins/substitute.lua
+++ lua/mario/plugins/substitute.lua
@@ -8,9 +8,9 @@ return {
 
     local keymap = vim.keymap
 
-    keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
-    keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
-    keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
-    keymap.set("x", "s", substitute.visual, { desc = "Substitute in visual mode" })
+    keymap.set("n", "gs", substitute.operator, { desc = "Substitute with motion" })
+    keymap.set("n", "gss", substitute.line, { desc = "Substitute line" })
+    keymap.set("n", "gS", substitute.eol, { desc = "Substitute to end of line" })
+    keymap.set("x", "gs", substitute.visual, { desc = "Substitute in visual mode" })
   end,
 }
